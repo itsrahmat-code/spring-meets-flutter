722f665e9e4d73821afb9fa7832a6edeeb8d8eb9
Rahmat Ullah <rahmatullahsaruk@gmail.com>
2025-10-11 13:39:58 +0600

    m

--- stock_management/stock_management/src/main/java/com/rahmatullahsaruk/stock_management/entity/Employee.java
+++
@@ -1,9 +0,0 @@
-package com.rahmatullahsaruk.stock_management.entity;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "employees")
-public class Employee {
-}
