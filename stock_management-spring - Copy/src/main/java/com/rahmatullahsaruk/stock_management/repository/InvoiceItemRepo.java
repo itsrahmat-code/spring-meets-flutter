17080c2906215ae29afae9b7c0877e6c4c0e64d9
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-10-26 16:10:33 +0600

    d

--- stock_management-spring - Copy/src/main/java/com/rahmatullahsaruk/stock_management/repository/InvoiceItemRepo.java
+++
@@ -1,9 +0,0 @@
-package com.rahmatullahsaruk.stock_management.repository;
-
-import com.rahmatullahsaruk.stock_management.entity.InvoiceItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InvoiceItemRepo extends JpaRepository<InvoiceItem, Long> {
-}
