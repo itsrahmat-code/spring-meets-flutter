6f914be889a4523d4e64c09977e94a211649c3ed
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-10-27 15:36:54 +0600

    d

--- New folder - Copy/stock_management-spring - Copy/src/main/java/com/rahmatullahsaruk/stock_management/entity/Role.java
+++
@@ -1,7 +0,0 @@
-package com.rahmatullahsaruk.stock_management.entity;
-    public enum Role {
-        ADMIN,
-        MANAGER,
-        CASHIER,
-    }
-
--- stock_management-spring - Copy/src/main/java/com/rahmatullahsaruk/stock_management/dto/YearProfitSummaryDTO.java
+++
@@ -1,8 +0,0 @@
-package com.rahmatullahsaruk.stock_management.dto;
-
-public class YearProfitSummaryDTO {
-    int year,
-    double revenue,
-    double expenses,
-    double profit
-}
