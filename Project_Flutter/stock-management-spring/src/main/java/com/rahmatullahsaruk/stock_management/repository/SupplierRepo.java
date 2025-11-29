7ed2a8cbfcfe4ce8a9328384c318ecbb749ce193
Rahmat Ullah <rahmatullahsaruk@gmail.com>
2025-11-29 20:29:29 +0600

    delete

--- Project_Flutter/stock-management-spring/src/main/java/com/rahmatullahsaruk/stock_management/repository/SupplierRepo.java
+++
@@ -1,9 +0,0 @@
-package com.rahmatullahsaruk.stock_management.repository;
-
-import com.rahmatullahsaruk.stock_management.entity.Supplier;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SupplierRepo extends JpaRepository<Supplier, Long> {
-}
